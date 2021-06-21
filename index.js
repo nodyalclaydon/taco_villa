6087c1c86d10a2a1d5f686de04e60f27fcf909c1
nodyalclaydon <72510779+nodyalclaydon@users.noreply.github.com>
2021-06-21 16:39:56 -0500

    added menu page

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ const navLink = document.querySelectorAll(".nav__link")
 menuBtn.addEventListener("click", () => {
     document.body.classList.toggle("nav-open")
 })
-
-navLink.forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.body.classList.toggle("nav-open")
-    })
-})
---
+++ menu.js
@@ -0,0 +1,18 @@
+const menuBoard = document.querySelector(".menu-entrees-container")
+
+async function getMenu() {
+    const data = await fetch("./menu.json")
+    const entrees = await data.json()
+    
+    entrees.forEach(item => {
+        menuBoard.innerHTML += `
+        <div class="entree-item">
+            <h2 class="entree-name">${item.entree}</h2>
+            <p class="entree-price">${item.price}</p>
+            <p class="entree-desc">${item.description}</p>
+        </div>
+        `
+    })
+}
+
+getMenu()

0031534035b5398da1f453952ba3de01b5f1df66
nodyalclaydon <72510779+nodyalclaydon@users.noreply.github.com>
2021-06-21 12:54:14 -0500

    home page styles

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const menuBtn = document.querySelector(".nav-toggle")
 const nav = document.querySelector(".nav")
+const navLink = document.querySelectorAll(".nav__link")
 
 menuBtn.addEventListener("click", () => {
     document.body.classList.toggle("nav-open")
-    console.log("click")
+})
+
+navLink.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.body.classList.toggle("nav-open")
+    })
 })
